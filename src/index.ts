b80e7c1f203cd5034eb8e8496131f86584156d46
syuilo <syuilotan@yahoo.co.jp>
2016-06-23 11:38:23 +0900

    :pizza:

--- src/index.ts
+++ src/index.ts
@@ -12,26 +12,35 @@ export class Color {
 	}
 
 	public toHex(withNumberSign: boolean = true): string {
-		// 16進数に変換
 		const rHex1 = this.r.toString(16);
 		const gHex1 = this.g.toString(16);
 		const bHex1 = this.b.toString(16);
 
-		// 0埋め
 		const rHex2 = `0${rHex1}`.slice(-2);
 		const gHex2 = `0${gHex1}`.slice(-2);
 		const bHex2 = `0${bHex1}`.slice(-2);
 
-		return withNumberSign ? `#${rHex2}${gHex2}${bHex2}` : rHex2 + gHex2 + bHex2;
+		return withNumberSign
+			? `#${rHex2}${gHex2}${bHex2}`
+			: rHex2 + gHex2 + bHex2;
 	}
 
-	public getLuminance(weights: {r: number, g: number, b: number} = {r: 0.298912, g: 0.586611, b: 0.114478}): number {
-		return Math.floor(
-			weights.r * this.r + weights.g * this.g + weights.b * this.b);
+	public luminance(rWeight = 0.2126, gWeight = 0.7152, bWeight = 0.0722): number {
+		return (
+			(rWeight * this.r / 255) +
+			(gWeight * this.g / 255) +
+			(bWeight * this.b / 255)
+		);
 	}
 
-	public getForegroundColor(threshold: number = 100, weights?: {r: number, g: number, b: number}): Color {
-		return this.getLuminance(weights) > threshold ? new Color(0, 0, 0) : new Color(255, 255, 255);
+	public contrast(threshold = 0.43, dark?: Color, light?: Color): Color {
+		return this.luminance() > threshold
+			? dark || new Color(0, 0, 0)
+			: light || new Color(255, 255, 255);
+	}
+
+	public invert(): Color {
+		return new Color(255 - this.r, 255 - this.g, 255 - this.b);
 	}
 }
 
@@ -39,7 +48,6 @@ const _ = (seed: string) => {
 	// Init randomizer
 	const random = seedrandom(seed);
 
-	// 各チャンネルをランダムに決定
 	const r = Math.floor(random() * 255);
 	const g = Math.floor(random() * 255);
 	const b = Math.floor(random() * 255);
